bd0d1ad003e0056c67e8b74b3072117956439b13
w4lto <jpbrasil373@gmail.com>
2023-01-25 14:55:39 -0300

    Removia a possibilidade de nota de 1 real

--- transdata-teste.py
+++ transdata-teste.py
@@ -13,14 +13,3 @@ for x in notas:
 
 #O codgio abaixo considera o valor de 1 real como cedula, sendo assim foi considerado
 
-"""
-print("Digite o valor a ser sacado")
-
-N = int(input())
-
-notas = [100,50,20,10,5,2,1]
-print ("R$ %d:"%(N))
-for x in notas:
-    print ("O valor sacado sera dividido nas seguintes notas %i nota(s) de R$ %.2f"%((N/x),x))
-    N %= (x)
-"""
